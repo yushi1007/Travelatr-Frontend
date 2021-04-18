7e10dc9f84c9dd3e7e8e36de233c24a1b26f7b98
Yu Shi <yushiys95@gmail.com>
2021-04-18 19:45:01 -0400

    wip

--- src/components/DestinationCard.js
+++ src/components/DestinationCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DestinationCard = () => {
-
+// Hello world
     return (
         <div className="destination-card">
             <h1>list of the place card</h1>
