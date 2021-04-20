f00a988aa6fd872356ca4edb8dcfe0e6d9d6f88b
Jamie H Pham <jpham1109@gmail.com>
2021-04-19 21:47:18 -0400

    Destination details partially done, photos bug

--- src/components/DestinationDetails.js
+++ src/components/DestinationDetails.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import FlightSearch from "./FlightSearch";
+import ReviewList from "./ReviewList";
+import PhotosContainer from "./PhotosContainer";
 
 const DestinationDetails = () => {
+    const [destination, setDestination] = useState([])
+    const {id} = useParams()
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/destinations/${id}`)
+        .then(r => r.json())
+        .then((destination) => setDestination(destination))
+    }, [id])
+    
+    const { name, image, description, us, airport_code, photos, rating, reviews, user_ratings_total } = destination
+    console.log(photos)
     return(
         <div className="destination-details">
-            <h1>Show one place details</h1>
+            <h3>{name}</h3>
+            <img src={image} alt={name}/>
+            <p>{description}</p>
+            <p>{us ? "Domestic" : "International"}</p>
+            <p>Airport code: {airport_code}</p>
+            <span style={{display: rating ? "block" : "none" }}>
+                <p>{rating}</p>
+                <p>{reviews ? <ReviewList reviews={reviews} /> : null }</p>
+                <p>{user_ratings_total} reviews </p>
+            </span>
+            {/* <PhotosContainer photos={photos} /> */}
+
             <FlightSearch />
         </div>
     )
---
+++ src/components/Photo.js
@@ -0,0 +1,5 @@
+export default function Photo ({photo}) {
+    return (
+        <img src={photo} alt="google"/>
+    )
+}
