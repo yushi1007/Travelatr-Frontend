c7ea0e00427c922aa885174db3d059bb75f7f167
Jamie H Pham <jpham1109@gmail.com>
2021-04-20 18:23:36 -0400

    Like working and persisting

--- src/components/DestinationDetails.js
+++ src/components/DestinationDetails.js
@@ -4,28 +4,48 @@ import FlightSearch from "./FlightSearch";
 import ReviewList from "./ReviewList";
 import PhotosContainer from "./PhotosContainer";
 
-const DestinationDetails = () => {
+const DestinationDetails = ({user}) => {
     const [destination, setDestination] = useState(null)
     const [isLoaded, setIsLoaded] = useState(false);
     const {id} = useParams()
-
+    
+    
     useEffect(() => {
-        fetch(`http://localhost:3000/destinations/${id}`)
+        fetch(`http://localhost:7000/destinations/${id}`)
         .then(r => r.json())
         .then((destination) => {
             setDestination(destination)
             setIsLoaded(true)
         })
     }, [id])
-
-    if (!isLoaded) return <h2>Loading...</h2>;
-
-    const { name, image, description, us, airport_code, photos, rating, reviews, user_ratings_total } = destination
     
+    
+    if (!isLoaded) return <h2>Loading...</h2>;
+    
+    const { name, image, description, us, airport_code, photos, rating, reviews, user_ratings_total, likes } = destination
+    
+    const handleLikeClick = () => {
+        fetch("http://localhost:7000/likes", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ user_id: user.id, destination_id: id })
+        })
+        .then(r => r.json())
+        .then(destination => {
+            setDestination(destination)
+            console.log(destination)
+        })
+    }
+
+
+
     return(
         <div className="destination-details">
             <h3>{name}</h3>
             <img src={image} alt={name}/>
+            <button className="like-btn" onClick={handleLikeClick}>{likes.length} Likes 💙</button>
             <p>{description}</p>
             <p>{us ? "Domestic" : "International"}</p>
             <p>Airport code: {airport_code}</p>
@@ -34,7 +54,7 @@ const DestinationDetails = () => {
                 <p>{reviews ? <ReviewList reviews={reviews} /> : null }</p>
                 <p>{user_ratings_total} reviews </p>
             </span>
-            {/* <PhotosContainer photos={photos} /> */}
+            <PhotosContainer photos={photos} />
             <FlightSearch />
         </div>
     )
